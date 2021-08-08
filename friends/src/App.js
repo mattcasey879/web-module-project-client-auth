d799854cc0fad7e141eb9bb191ff0082a113cba7
Matt <matthew-casey@lambdastudents.com>
2021-08-07 19:15:26 -0500

    matthew-casey

---
+++ friends/src/App.js
@@ -0,0 +1,16 @@
+import { Link, Switch, Route } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
